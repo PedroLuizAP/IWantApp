6f95ea7060a2601f7a1e692323da735f5e2e0917
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-02-13 18:00:11 -0300

    adjust Product

--- IWantApp/Domain/Products/Product.cs
+++ IWantApp/Domain/Products/Product.cs
@@ -6,5 +6,6 @@
         public string Description { get; set; }
         public bool HasStock { get; set; }
         public Category Category { get; set; }
+        public long CategoryId { get; set; }
     }
 }
