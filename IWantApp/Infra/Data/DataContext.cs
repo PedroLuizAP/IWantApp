3239cb87e25c7ba7b0cd056a875746c968006eee
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-02-13 18:00:33 -0300

    adjust DbContext

--- IWantApp/Infra/Data/DataContext.cs
+++ IWantApp/Infra/Data/DataContext.cs
@@ -1,6 +1,25 @@
-﻿namespace IWantApp.Infra.Data
+﻿using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace IWantApp.Infra.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating (ModelBuilder builder)
+        {
+            builder.Entity<Product>().Property(p => p.Description).HasMaxLength(255);
+            builder.Entity<Product>().Property(p => p.Name).HasMaxLength(120).IsRequired();
+            builder.Entity<Category>().Property(p => p.Name).HasMaxLength(20).IsRequired();
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configuration)
+        {
+            configuration.Properties<string>().HaveMaxLength(100);
+        }
     }
 }
