1b3fe4a0190220f65163c31ba249de59f71c8d94
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-03-02 10:16:57 -0300

    create GetAll and Put endpoints

---
+++ IWantApp/Endpoints/Categories/CategoryGetAll.cs
@@ -0,0 +1,22 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories
+{
+    public class CategoryGetAll
+    {
+        public static string Template => "/categories";
+        public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+
+        public static Delegate Handle => Action;
+
+        public static IResult Action(DataContext context)
+        {
+            var categories = context.Category.ToList();
+
+            var response = categories.Select(c => new CategoryResponse { Id = c.Id, Active = c.Active, Name = c.Name });
+
+            return Results.Ok(response);
+        }
+    }
+}
---
+++ IWantApp/Endpoints/Categories/CategoryPut.cs
@@ -0,0 +1,27 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IWantApp.Endpoints.Categories
+{
+    public class CategoryPut
+    {
+        public static string Template => "/categories/{id}";
+        public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
+
+        public static Delegate Handle => Action;
+
+        public static IResult Action([FromRoute] Guid id, CategoryRequest  categoryRequest, DataContext context)
+        {
+            var category = context.Category.Where(c => c.Id == id).FirstOrDefault();
+
+            category.Active = categoryRequest.Active;
+            category.Name = categoryRequest.Name;
+
+            context.Category.Update(category);
+            context.SaveChanges();
+
+            return Results.Ok();
+        }
+}
+}
