bbce52ff719f45a3f6e511e926cb9bd3782d229c
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-03-17 08:23:31 -0300

    create ProblemDetailsExtensions

---
+++ IWantApp/Endpoints/ProblemDetailsExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Flunt.Notifications;
+using System.Runtime.CompilerServices;
+
+namespace IWantApp.Endpoints
+{
+    public static class ProblemDetailsExtensions
+    {
+        public static Dictionary<string, string[]> ConvertToProblemDetails(this IReadOnlyCollection<Notification> notification) => notification.GroupBy(g => g.Key).ToDictionary(d => d.Key, d=> d.Select(x => x.Message).ToArray());
+    }
+}
