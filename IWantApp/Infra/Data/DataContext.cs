fcd06237df32bad60056f1923745892568c6a486
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-03-19 00:45:20 -0300

    update Database

--- IWantApp/Infra/Data/DataContext.cs
+++ IWantApp/Infra/Data/DataContext.cs
@@ -14,6 +14,8 @@ namespace IWantApp.Infra.Data
         public DataContext(DbContextOptions options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+
             builder.Ignore<Notification>();
 
             builder.Entity<Product>().Property(p => p.Description).HasMaxLength(255);
