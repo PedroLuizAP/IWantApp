1977056bd6e5595c165b18d31cbe220d4eb763c0
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-04-16 13:07:00 -0300

    adjust CategoryPut

--- IWantApp/Endpoints/Categories/CategoryPut.cs
+++ IWantApp/Endpoints/Categories/CategoryPut.cs
@@ -1,6 +1,9 @@
 ﻿using IWantApp.Domain.Products;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IWantApp.Endpoints.Categories
 {
@@ -8,16 +11,18 @@ namespace IWantApp.Endpoints.Categories
     {
         public static string Template => "/categories/{id:guid}";
         public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
-
         public static Delegate Handle => Action;
 
-        public static IResult Action([FromRoute] Guid id, CategoryRequest  categoryRequest, DataContext context)
+        [Authorize(Policy = "EmplyeePolicy")]
+        public static IResult Action([FromRoute] Guid id, CategoryRequest categoryRequest, HttpContext http, DataContext context)
         {
+            var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
             var category = context.Category.Where(c => c.Id == id).FirstOrDefault();
 
             if (category == null) Results.NotFound();
 
-            category!.EditInfo(categoryRequest.Name, categoryRequest.Active);
+            category!.EditInfo(categoryRequest.Name, categoryRequest.Active, userId);
 
             if (!category.IsValid) return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
 
@@ -27,5 +32,5 @@ namespace IWantApp.Endpoints.Categories
 
             return Results.Ok();
         }
-}
+    }
 }
