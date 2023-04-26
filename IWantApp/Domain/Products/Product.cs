b8552706f2416bc46b13bc8563de518476e8148d
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-04-26 08:49:08 -0300

    create Product

--- IWantApp/Domain/Products/Product.cs
+++ IWantApp/Domain/Products/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace IWantApp.Domain.Products
+﻿using Flunt.Validations;
+
+namespace IWantApp.Domain.Products
 {
     public class Product : Entity
     {
@@ -9,5 +11,31 @@
         public long CategoryId { get; set; }
         public bool Active { get; set; } = true;
 
+        public Product(string name, Category category, string description, bool hasStock, string createdBy)
+        {
+            Name = name;
+            Category = category;
+            Description = description;
+            HasStock = hasStock;
+            CreatedBy = createdBy;
+            EditedBy = createdBy;
+
+            CreatedOn = DateTime.Now;
+            EditedOn = DateTime.Now;
+        }
+
+        private void Validate()
+        {
+            var contract = new Contract<Product>()
+                .IsNotNullOrEmpty(Name, "Name")
+                .IsGreaterOrEqualsThan(Name, 3, "Name")
+                .IsNotNullOrEmpty(Description, "Description")
+                .IsNotNull(Category, "Category")
+                .IsGreaterOrEqualsThan(Description, 3 , "Description")
+                .IsNotNullOrEmpty(CreatedBy, "CreatedBy")
+                .IsNotNullOrEmpty(EditedBy, "EditedBy");
+
+            AddNotifications(contract);
+        }
     }
 }
