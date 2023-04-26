d81313c6f7a24fea6fe8e7d9f0444e2ba43f9939
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-04-26 08:48:50 -0300

    create ProductResponse

---
+++ IWantApp/Endpoints/Products/ProductResponse.cs
@@ -0,0 +1,7 @@
+﻿using IWantApp.Domain.Products;
+
+namespace IWantApp.Endpoints.Products
+{
+    public record ProductResponse(string Name, Category Category, string Description, bool HasStock, bool Active);
+    
+}
