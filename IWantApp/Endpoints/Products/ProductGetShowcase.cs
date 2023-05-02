80484b6bf41630728d7ce92fffa6b810f4bbcd9a
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-05-02 17:44:19 -0300

    adjust pages/rows ProductGetShowcase

--- IWantApp/Endpoints/Products/ProductGetShowcase.cs
+++ IWantApp/Endpoints/Products/ProductGetShowcase.cs
@@ -12,9 +12,22 @@ namespace IWantApp.Endpoints.Products
 
         public static Delegate Handle = Action;
         [AllowAnonymous]
-        public static async Task<IResult> Action(DataContext context)
+        public static async Task<IResult> Action(int? page, int? row, string orderBy, DataContext context)
         {
-            var products = context.Products.Include(p => p.Category).Where(p => p.HasStock && p.Category.Active).OrderBy(p => p.Name).ToList();
+            page ??= 1;
+
+            row ??= 10;
+
+            if (string.IsNullOrEmpty(orderBy)) orderBy = "name";
+
+            var queryBase = context.Products.Include(p => p.Category).Where(p => p.HasStock && p.Category.Active);
+
+            var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+
+            if (orderBy == "name") queryFilter = queryFilter.OrderBy(p => p.Name);
+            else queryFilter = queryFilter.OrderBy(p => p.Price);
+
+            var products = queryFilter.ToList();
 
             var results = products.Select(p => new ProductResponse(p.Name, p.Category, p.Description, p.HasStock, p.Price, p.Active));
 
