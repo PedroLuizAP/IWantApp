311f1d4c56482156b38e71a6f44d3a155e198c90
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-05-08 11:52:25 -0300

    refactor EmployeePost

--- IWantApp/Endpoints/Employees/EmployeePost.cs
+++ IWantApp/Endpoints/Employees/EmployeePost.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IWantApp.Domain.Users;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 
@@ -11,28 +12,22 @@ namespace IWantApp.Endpoints.Employees
         public static Delegate Handle => Action;
 
         [Authorize(Policy = "EmplyeePolicy")]
-        public static async Task<IResult> Action(EmployeeRequest employeeRequest, HttpContext http, UserManager<IdentityUser> userManager)
+        public static async Task<IResult> Action(EmployeeRequest employeeRequest, HttpContext http, UserCreator userCreator)
         {
-            var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-
-            var newUser = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
-
-            var result = await userManager.CreateAsync(newUser, employeeRequest.Password);
-
-            if (!result.Succeeded) return Results.ValidationProblem(result.Errors.ConvertToProblemDetails());
+            var createBy = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
 
             var userClaims = new List<Claim>
             {
                 new Claim("EmployeeCode", employeeRequest.EmployeeCode),
                 new Claim("Name", employeeRequest.Name),
-                new Claim("CreatedBy", userId)
+                new Claim("CreatedBy", createBy)
             };
 
-            var claimResult = await userManager.AddClaimsAsync(newUser, userClaims);
+            var (result, userId) = await userCreator.Create(employeeRequest.Email, employeeRequest.Password, userClaims);
 
-            if (!claimResult.Succeeded) return Results.BadRequest(claimResult.Errors.First());
+            if (!result.Succeeded) return Results.ValidationProblem(result.Errors.ConvertToProblemDetails());
 
-            return Results.Created($"/employees/{newUser.Id}", newUser.Id);
+            return Results.Created($"/employees/{userId}", userId);
         }
     }
 }
