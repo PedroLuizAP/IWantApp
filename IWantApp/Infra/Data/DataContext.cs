8e366451c3abbabcb751a5d01de4dfb709d0c142
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-05-11 08:00:23 -0300

    adjust DataContext

--- IWantApp/Infra/Data/DataContext.cs
+++ IWantApp/Infra/Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Flunt.Notifications;
+using IWantApp.Domain.Orders;
 using IWantApp.Domain.Products;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -10,6 +11,7 @@ namespace IWantApp.Infra.Data
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Category { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         public DataContext(DbContextOptions options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder builder)
@@ -21,7 +23,11 @@ namespace IWantApp.Infra.Data
             builder.Entity<Product>().Property(p => p.Description).HasMaxLength(255);
             builder.Entity<Product>().Property(p => p.Name).HasMaxLength(120).IsRequired();
             builder.Entity<Product>().Property(p => p.Price).HasColumnType("decimal(10,2)").IsRequired();
-            builder.Entity<Category>().Property(p => p.Name).HasMaxLength(20).IsRequired();
+            builder.Entity<Category>().Property(c => c.Name).HasMaxLength(20).IsRequired();
+            builder.Entity<Order>().Property(o => o.ClientId).IsRequired();
+            builder.Entity<Order>().Property(o => o.DeliveryAddress).IsRequired();
+            builder.Entity<Order>().HasMany(o => o.Products).WithMany(p => p.Orders)
+                .UsingEntity(x => x.ToTable("OrderProducts"));
         }
 
         protected override void ConfigureConventions(ModelConfigurationBuilder configuration)
