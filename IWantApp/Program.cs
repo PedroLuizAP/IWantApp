e3a76b96e4dcabbdd9ae3bc3d6925070959435e2
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-05-13 13:02:26 -0300

    adjust Policy

--- IWantApp/Program.cs
+++ IWantApp/Program.cs
@@ -51,7 +51,7 @@ builder.Services.AddAuthorization(options =>
     options.FallbackPolicy = new AuthorizationPolicyBuilder().AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme).RequireAuthenticatedUser().Build();
 #endif
     options.AddPolicy("EmployeePolicy", p => p.RequireAuthenticatedUser().RequireClaim("EmployeeCode"));
-    options.AddPolicy("Employee005Policy", p => p.RequireAuthenticatedUser().RequireClaim("EmployeeCode", "005"));
+    options.AddPolicy("DocumentPolicy", p => p.RequireAuthenticatedUser().RequireClaim("Document"));
 });
 
 
