c15a85651eac8c34b2638935969d1c1dda5485ca
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-05-13 13:02:42 -0300

    fix OrderPost

--- IWantApp/Endpoints/Orders/OrderPost.cs
+++ IWantApp/Endpoints/Orders/OrderPost.cs
@@ -12,16 +12,20 @@ namespace IWantApp.Endpoints.Orders
         public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
         public static Delegate Handle => Action;
 
-        [Authorize]
+        [Authorize(Policy = "DocumentPolicy")]
         public static async Task<IResult> Action(OrderRequest orderRequest, HttpContext http, DataContext context)
         {
             var clientId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
 
             var clientName = http.User.Claims.First(c => c.Type == "Name").Value;
 
-            var products = context.Products.Where(p => orderRequest.Products.Contains(p.Id)).ToList();
+            List<Product> products = new ();
 
-            var order = new Order(clientId, clientName, products, orderRequest.DeliveryAddress);
+            if(orderRequest?.Products?.Count > 0)products = context.Products.Where(p => orderRequest.Products.Contains(p.Id)).ToList();
+
+            var order = new Order(clientId, clientName, products, orderRequest!.DeliveryAddress);
+
+            if(!order.IsValid) return Results.ValidationProblem(order.Notifications.ConvertToProblemDetails());
 
             await context.Orders.AddAsync(order);
 
